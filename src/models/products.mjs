8e83a82c3bc4b06e886e486d9e4339e11ab845d7
italo <italosport110@hotmail.com>
2023-07-07 16:33:02 -0300

    change type price

--- src/models/products.mjs
+++ src/models/products.mjs
@@ -1,32 +1,25 @@
 import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    description: { type: String },
-    idCategory: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
-      required: true,
-    },
-    price: {
-      type: Number,
-      get: (v) => (v / 100).toFixed(2),
-      set: (v) => v * 100,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  idCategory: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
   },
-  {
-    toJSON: { getters: true },
-  }
-);
+  price: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
 
 ProductSchema.index({ name: 1 });
 
